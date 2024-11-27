6b8226c9f9e1e4bafd29e24f9945f8cbdeaa719c
James Junaidi <jamesjunaidi@Jamess-MacBook-Pro.local>
2024-11-27 00:02:19 -0800

    Main activity cleanup

--- app/src/main/java/com/junaidi/BibleAddict/MainActivity.kt
+++ app/src/main/java/com/junaidi/BibleAddict/MainActivity.kt
@@ -3,14 +3,12 @@ package com.junaidi.BibleAddict
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.Text
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.junaidi.BibleAddict.ui.theme.BibleAddictTheme
 import com.junaidi.BibleAddict.Models.Profile
 import com.junaidi.BibleAddict.maincomposables.Heading
@@ -31,32 +29,15 @@ class MainActivity : ComponentActivity() {
                         item {
                             Heading(Profile(5, 5, "James"))
                         }
-
                         item {
                             WeeklyCheckboxes()
                         }
-
                         item {
-                            Text("Notification Selector")
+                            Text("Notification Selector") // Placeholder for notification section
                         }
                     }
-
-                    // Greeting("Android")
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    BibleAddictTheme {
-        Greeting("Android")
     }
 }
